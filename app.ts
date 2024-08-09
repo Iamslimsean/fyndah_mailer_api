1ed7455e4ba70a6c124655fb506e478a2444243b
Iamslimsean <swadcapital@gmail.com>
2024-08-09 04:06:45 +0100

    curly vra

--- app.ts
+++ app.ts
@@ -37,7 +37,7 @@ const StartServer = () => {
   // Cors
   app.use(
     cors({
-      origin: ["https://fyndahmailerauth.vercel.app"],
+      origin: "https://fyndahmailerauth.vercel.app",
       // ["https://fyndahmailer.vercel.app", "https://fyndahmailerauth.vercel.app"],
       credentials: true,
       methods: ["POST"],
